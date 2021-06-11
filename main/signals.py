47690caac41c20db3cf67c4483d1a1d6ff982880
Md Suhaib Safwan <suhaibsafwan45@gmail.com>
2021-06-11 16:26:38 +0600

    Created THe Signals for sending Email...

--- main/signals.py
+++ main/signals.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import Profile, VerificationToken
+from .models import Profile, VerificationToken, ForgotPasswordToken
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 
@@ -16,7 +16,8 @@ def create_profile_model(sender, instance, created, **kwargs):
 @receiver(signal=post_save, sender=Profile)
 def email_verification_token(sender, instance, created, **kwargs):
     if created:
-        obj = VerificationToken.objects.create(user=instance.user)
+        user = instance.user
+        obj = VerificationToken.objects.create(user=user)
         msg_cont = {
             "token": obj.token
         }
@@ -26,8 +27,28 @@ def email_verification_token(sender, instance, created, **kwargs):
             subject="Confirmation Of Account",  
             from_email="djangoemailsafwan@gmail.com",
             message=msg, 
-            recipient_list=["suhaibsafwan45@gmail.com"],
+            recipient_list=[user.email, ],
             html_message=msg_html
         )
         
         return obj
+
+
+@receiver(signal=post_save, sender=ForgotPasswordToken)
+def send_forgot_password_email(sender, instance, created, **kwargs):
+    if created:
+        token = instance.token
+        msg_cont = {
+            "token": token,
+        }
+        msg_html = render_to_string("Extras/confirmation.html", msg_cont)
+        msg = "Recover your Account Here"
+        send_mail(
+            subject="Recover Your Account!",
+            from_email="djangoemailsafwan@gmail.com",
+            message=msg,
+            recipient_list=[instance.user.email, ],
+            html_message=msg_html
+        )
+
+        return instance
